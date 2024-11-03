b722413dd781d96e1591a126d06054dca35f167d
ejf <john@ejf.io>
2024-11-03 15:46:45 -0800

    In-progress refactor of Akai APC code
    This is currently not working, and needs some continued effort to fix.

--- zyngine/ctrldev/__init__.py
+++ zyngine/ctrldev/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 __all__ = [
+    "zynthian_ctrldev_akai_apc_generic",
     "zynthian_ctrldev_akai_apc_key25_mk2",
     "zynthian_ctrldev_akai_apc_mini_mk2",
     "zynthian_ctrldev_akai_midimix",
